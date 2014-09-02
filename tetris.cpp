6d517ca39f48d83b3623cca2374e52ef343571d0
wuwing <616011410@qq.com>
2014-09-03 01:01:56 +0800

    try

---
+++ tetris.cpp
@@ -0,0 +1,13 @@
+/******************************************************
+ * File Name:   tetris.cpp
+ * Author:      kojimai
+ * Creater Time:2014年09月03日 星期三 01时00分51秒
+******************************************************/
+
+#include "wx/wx.h"
+
+class Tetris :public wxFrame
+{
+	public:
+
+}
